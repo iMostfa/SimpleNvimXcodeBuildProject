67499294498050805fcfa55f8382ccd4db7404a2
iMostafa <mostafa.essam@wallapop.com>
2024-11-06 17:31:30 +0200

    unused stuff

--- build/Pods.build/Debug-iphonesimulator/Alamofire.build/Objects-normal/arm64/Alamofire-dummy.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/mostafa.essam/Desktop/Simplenvimproblem/Pods/Target\ Support\ Files/Alamofire/Alamofire-dummy.m \
-  /Users/mostafa.essam/Desktop/Simplenvimproblem/Pods/Target\ Support\ Files/Alamofire/Alamofire-prefix.pch \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/System/Library/Frameworks/UIKit.framework/Modules/module.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/System/Library/Frameworks/Foundation.framework/Modules/module.modulemap
